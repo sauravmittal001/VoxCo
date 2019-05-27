6cb879c46b9f692e452aa66e7102cf33ce7a63f3
sauravmittal001 <sauravmittal001@gmail.com>
2019-05-27 23:31:44 +0530

    raw project built

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Indoor.java
@@ -0,0 +1,16 @@
+npackage com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Indoor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indoor);
+    }
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity { //changed "extends AppCompatActivity" to "extends Activity"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button START_CONFERENCE_BUTTON = (Button) findViewById(R.id.START_CONFERENCE_BUTTON);
+
+        START_CONFERENCE_BUTTON.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, Conference.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
