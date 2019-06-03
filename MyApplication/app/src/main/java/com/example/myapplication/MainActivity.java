ae7cfc827be068055b17148ec6c4d73a35030dbf
sauravmittal001 <sauravmittal001@gmail.com>
2019-06-04 00:20:17 +0530

    my message

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //public static final android.util.Log Log = ; // add
     Timer timer;
 
     @Override
@@ -17,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Log.d("myTag", "This is my message"); // add
+
         timer = new Timer();
+
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -27,5 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }, 500);
+
     }
 }
