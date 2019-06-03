e9a9ae9dcb66fba49c8d567c4f088fd3075f733a
Prashant Mittal <prashant.mittal@rivigo.com>
2019-06-03 22:45:44 +0530

    fixed saurav's issues

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,30 +2,26 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
             @Override
             public void run() {
                 Intent indent = new Intent(MainActivity.this, IndexActivity.class);
                 startActivity(indent);
-                finish();
-
             }
-        }, 500);
+        }, 1000L);
+
     }
 }
