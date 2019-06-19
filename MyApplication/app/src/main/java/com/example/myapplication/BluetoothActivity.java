7ad531ef593905a9aef04d74e3fdb7107aaeaeee
sauravmittal001 <sauravmittal001@gmail.com>
2019-06-19 21:32:46 +0530

    made complete connect button (listview error to be rendered)

--- MyApplication/app/src/main/java/com/example/myapplication/BluetoothActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/BluetoothActivity.java
@@ -1,15 +1,23 @@
 package com.example.myapplication;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 
 public class BluetoothActivity extends AppCompatActivity {
+    ArrayAdapter aAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,7 @@ public class BluetoothActivity extends AppCompatActivity {
                 } else {
                     if (!bluetoothAdapter.isEnabled()) {
                         startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 1);
-                        Toast.makeText(getApplicationContext(), "VoxCo Connection Turned ON", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Turning ON VoxCo Connection", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -40,6 +48,43 @@ public class BluetoothActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "VoxCo Connection Turned OFF", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        Button buttonPairedObject = findViewById(R.id.buttonPaired);
+        buttonPairedObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetoothAdapter == null) {
+                    Toast.makeText(getApplicationContext(), "Bluetooth Not Supported", Toast.LENGTH_SHORT).show();
+                } else {
+                    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                    ArrayList<String> list = new ArrayList<>();
+                    if (pairedDevices.size() > 0) {
+                        for (BluetoothDevice device : pairedDevices) {
+                            String deviceName = device.getName();
+                            String macAddress = device.getAddress();
+                            int somethings = device.getBondState();
+                            list.add("Name: " + deviceName + "\n\nMAC Address: " + macAddress + "\nBond State: " + somethings + "\n");
+                        }
+                        ListView listView = findViewById(R.id.listviewPairedDevices);
+                        aAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+                        listView.setAdapter(aAdapter);
+                    }
+                }
+            }
+        });
+
+        Button buttonDiscoverObject = findViewById(R.id.buttonDiscover);
+
+        buttonDiscoverObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(BluetoothActivity.this, DiscoverActivity.class);
+                BluetoothActivity.this.startActivity(myIntent);
+            }
+        });
+
     }
+
 
 }
