fe46c3ec752682ed0c94ba75834ca1765990eafc
sauravmittal001 <sauravmittal001@gmail.com>
2019-06-26 21:59:50 +0530

    final app prepared made before Processor involvement

--- MyApplication/app/src/main/java/com/example/myapplication/SaveActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SaveActivity.java
@@ -1,11 +1,10 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SaveActivity extends AppCompatActivity {
 
@@ -14,20 +13,31 @@ public class SaveActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save);
         getWindow().getDecorView().setBackgroundColor(Color.WHITE);
-        Button buttonCancelObject = findViewById(R.id.buttonCancel);
+        /*Button buttonCancelObject = findViewById(R.id.buttonCancel);
 
         buttonCancelObject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Intent myIntent = new Intent(SaveActivity.this, ConferenceActivity.class);
-                SaveActivity.this.startActivity(myIntent);*/
+                //Intent myIntent = new Intent(SaveActivity.this, ConferenceActivity.class);
+                //SaveActivity.this.startActivity(myIntent);
                 finish();
 
             }
+        });*/
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+
+        ImageButton BackButtonObject = findViewById(R.id.BackButton);
+
+        BackButtonObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
         });
-
-
     }
+
+
 }
 
 //TODO: The Details should get saved with the file.
